b13090c7d2146fa1567186ada83e62d39e818a40
Celal <celaltok2@gmail.com>
2021-12-20 19:38:03 +0300

    git init

---
+++ DynamicTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicTableViewCell
+//
+//  Created by MCT on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
